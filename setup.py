1988070a25d09a16b8e34391ace395fb91b6da01
Guillaume de Chambrier <chambrierg@gmail.com>
2016-10-26 13:35:14 +0200

    added description

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(name='gym_leftright',
       version='0.0.1',
       author='Guillaume de Chambrier',
       author_email='chambrierg@gmail.com',
+      description='Left or right control environment for openai/gym',
+      url='https://github.com/gpldecha/gym-leftright',
+      license='MIT',
+      packages=['gym_leftright'],
       install_requires=['gym']
 )
