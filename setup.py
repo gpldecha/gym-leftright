2f5d1fcdaf9f546079460a4aff668576783dbe2b
Guillaume de Chambrier <chambrierg@gmail.com>
2017-01-16 15:11:02 +0100

    added find_packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='gym_leftright',
-      version='0.0.1',
+      version='0.0.2',
       author='Guillaume de Chambrier',
       author_email='chambrierg@gmail.com',
       description='Left or right control environment for openai/gym',
